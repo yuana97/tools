11a8d4b6140f3a0e88e98d45d4ccc8804d7ce51d
Allen Yuan <alleyuan@amazon.com>
2022-04-25 00:56:47 -0700

    table2csv scraper

---
+++ pythonscripts/tabletocsv_scraper.py
@@ -0,0 +1,52 @@
+# getting around 403:
+# inspect requests, copy useragent and pass as -ua arg
+# e.g. Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36
+# converts table to csv
+
+# todo: generalize
+
+import getopt
+import requests
+import sys
+import pandas as pd
+from bs4 import BeautifulSoup # pip install beautifulsoup4
+from urllib.parse import urlparse
+
+if __name__ == "__main__":
+  try:
+      opts, args = getopt.getopt(sys.argv[1:], "u:ua:", ["url="])
+  except getopt.GetoptError as err:
+      print(err)
+      sys.exit()
+  for param, val in opts:
+      if param in ('-u', '--url'):
+          try:
+              url = val
+              assert(urlparse(url))
+          except AssertionError:
+              print("URL (e.g. https://www.google.com) must be valid. Make sure argument is -u or --url.")
+              sys.exit()
+      else:
+          print("--url (e.g. https://www.google.com) are the only possible commandline arguments.")
+          sys.exit()
+  try:
+      print("Getting HTML document for url: " + url)
+  except NameError:
+      print("--url (e.g. https://www.google.com) is a required commandline argument.")
+      sys.exit()
+
+  headers = {
+    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
+  }
+
+  response = requests.get(url, headers=headers)
+  soup = BeautifulSoup(response.content, "html.parser")
+
+  tables = soup.find_all('table', 'table')
+  
+  df = pd.read_html(str(tables[0]))
+  df = df[0]
+  print(df)
+  df.to_csv('tabletocsv_scraper_output.csv')
+  print("successfully saved dataframe to csv at: " + "tabletocsv_scraper_output.csv")
+
